9a57d4d2180f3800afa6f23f78a2eedc4f2e4235
Owen Bickford <owen+github@owencode.com>
2022-08-28 21:54:18 -0400

    use cose key, add constraints and defaults

--- lib/demo/authentication/user_key.ex
+++ lib/demo/authentication/user_key.ex
@@ -2,14 +2,15 @@ defmodule Demo.Authentication.UserKey do
   use Ecto.Schema
   import Ecto.Changeset
   alias Demo.Accounts.User
+  alias WebAuthnLiveComponent.CoseKey
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "user_keys" do
     field :key_id, :binary
-    field :label, :string
+    field :label, :string, default: "default"
     field :last_used, :utc_datetime
-    field :public_key, :binary
+    field :public_key, CoseKey
     belongs_to :user, User
 
     timestamps()
@@ -20,6 +21,8 @@ defmodule Demo.Authentication.UserKey do
     user_key
     |> cast(attrs, [:user_id, :key_id, :public_key, :label, :last_used])
     |> validate_required([:user_id, :key_id, :public_key, :label])
+    |> unique_constraint([:key_id])
+    |> unique_constraint([:user_id, :label])
   end
 
   @doc false
