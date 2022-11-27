d8f223a50d7e339c47532cbd426c7f4270e3f4ed
Owen Bickford <owen+github@owencode.com>
2022-11-27 13:42:06 -0600

    update lib dir

--- lib/demo_web/controllers/page_controller.ex
+++ lib/demo_web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule DemoWeb.PageController do
   use DemoWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html", page_title: "Home")
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
   end
 end
--- lib/demo_web/endpoint.ex
+++ lib/demo_web/endpoint.ex
@@ -7,7 +7,8 @@ defmodule DemoWeb.Endpoint do
   @session_options [
     store: :cookie,
     key: "_demo_key",
-    signing_salt: "IPhATtXK"
+    signing_salt: "ILV+xzyq",
+    same_site: "Lax"
   ]
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
@@ -20,7 +21,7 @@ defmodule DemoWeb.Endpoint do
     at: "/",
     from: :demo,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: DemoWeb.static_paths()
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
