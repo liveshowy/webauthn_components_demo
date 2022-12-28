34b839cb96dad3bff03318ccd21fc5e5a7032202
Owen Bickford <owen@owencode.com>
2022-12-27 21:19:56 -0600

    update user and user key schemas

--- lib/demo/authentication/user_key.ex
+++ lib/demo/authentication/user_key.ex
@@ -8,22 +8,29 @@ defmodule Demo.Authentication.UserKey do
   @foreign_key_type :binary_id
   @derive {Jason.Encoder, only: [:key_id, :public_key, :label, :last_used]}
   schema "user_keys" do
-    field :key_id, :binary
     field :label, :string, default: "default"
-    field :last_used, :utc_datetime
+    field :key_id, :binary
+    field :user_handle, :binary
     field :public_key, CoseKey
     belongs_to :user, User
+    field :last_used, :utc_datetime
 
     timestamps()
+  end
+
+  def changeset(user_key, attrs \\ %{}) do
+    user_key
+    |> cast(attrs, [:user_id, :key_id, :public_key, :label, :user_handle])
   end
 
   @doc false
   def new_changeset(user_key, attrs) do
     user_key
-    |> cast(attrs, [:user_id, :key_id, :public_key, :label, :last_used])
-    |> validate_required([:user_id, :key_id, :public_key, :label])
-    |> update_change(:key_id, &Base.decode64!(&1, padding: false))
+    |> cast(attrs, [:user_id, :key_id, :public_key, :label, :user_handle])
+    |> validate_required([:user_id, :key_id, :public_key, :label, :user_handle])
     |> foreign_key_constraint(:user_id)
+    |> put_change(:last_used, get_timestamp())
+    |> unique_constraint([:user_handle], message: "key already registered")
     |> unique_constraint([:key_id], message: "key already registered")
     |> unique_constraint([:user_id, :label], message: "label already taken")
   end
@@ -31,8 +38,14 @@ defmodule Demo.Authentication.UserKey do
   @doc false
   def update_changeset(user_key, attrs) do
     user_key
-    |> cast(attrs, [:label, :last_used])
+    |> cast(attrs, [:label])
     |> validate_required([:label])
     |> unique_constraint([:user_id, :label], message: "label already taken")
+    |> put_change(:last_used, get_timestamp())
+  end
+
+  def get_timestamp do
+    DateTime.utc_now()
+    |> DateTime.truncate(:second)
   end
 end
