f351fcea077dd53e4b662c5e981d54c789c3fbe7
Owen Bickford <owen@owencode.com>
2023-09-18 19:40:24 -0500

    add `webauthn_components`

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Demo.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:webauthn_components, "~> 0.6"}
     ]
   end
 
