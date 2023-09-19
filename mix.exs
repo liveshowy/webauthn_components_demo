bd63f08e73b8a23340ff37e048d8be4c4286fbab
Owen Bickford <owen@owencode.com>
2023-09-18 19:53:36 -0500

    use local dep

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Demo.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:webauthn_components, "~> 0.6"}
+      {:webauthn_components, path: "../webauthn_components"}
+      # {:webauthn_components, "~> 0.6"}
     ]
   end
 
