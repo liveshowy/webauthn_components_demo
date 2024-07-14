0f3ba6222b80ba9cc449a4a77cb7b06d8f5c565d
Owen Bickford <owen@smartlogic.io>
2024-07-14 15:51:14 -0500

    Revert "`mix wac.install` v0.7.3"
    This reverts commit 51b819bc1df76e1a53e185970432475c10a84685.

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,19 +20,12 @@ import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
-import {
-  SupportHook,
-  AuthenticationHook,
-  RegistrationHook,
-} from "webauthn_components";
-
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken},
-hooks: { SupportHook, AuthenticationHook, RegistrationHook }
+  params: {_csrf_token: csrfToken}
 })
 
 // Show progress bar on live navigation and form submits
