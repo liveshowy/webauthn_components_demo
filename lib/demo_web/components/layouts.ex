a0ec83f66f1285a6860f50824c386d36a9169e22
Owen Bickford <owen@smartlogic.io>
2024-07-14 15:09:38 -0500

    `mix phx.new . --app demo --module Demo`

---
+++ lib/demo_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule DemoWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use DemoWeb, :controller` and
+  `use DemoWeb, :live_view`.
+  """
+  use DemoWeb, :html
+
+  embed_templates "layouts/*"
+end
